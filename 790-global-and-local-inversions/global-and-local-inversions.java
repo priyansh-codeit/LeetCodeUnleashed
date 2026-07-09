592ed416754e9a0258ce379b2254eda4459db036
priyansh-codeit <y.priyansh2438@gmail.com>
2026-07-10 01:33:56 +0530

    Time: 1 ms (93.12%) | Memory: 65.5 MB (43.12%) - LeetSync

---
+++ 790-global-and-local-inversions/global-and-local-inversions.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public boolean isIdealPermutation(int[] A) {
+        for (int i = 0;i<A.length; i++)
+            if (i-A[i]>1 || i-A[i]<-1) return false;
+        return true;
+    }
+}

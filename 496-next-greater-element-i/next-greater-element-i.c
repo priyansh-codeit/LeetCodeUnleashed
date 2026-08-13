df23788efe1946493766effff91558caccd363d9
Priyansh Yadav <y.priyansh2438@gmail.com>
2026-08-13 20:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (23.70%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.c
+++ 496-next-greater-element-i/next-greater-element-i.c
@@ -1,22 +1,22 @@
 int* nextGreaterElement(int* nums1,int nums1Size,int* nums2,int nums2Size,int* returnSize){
-    int* ans=malloc(nums1Size*sizeof(int));
-    *returnSize=nums1Size;
+    int *ans=malloc(nums1Size*sizeof(int));
+    int stack[1000],top=-1;
+    int map[10001];
 
-    for(int i=0;i<nums1Size;i++){
-        ans[i]=-1;
+    for(int i=0;i<10001;i++)
+        map[i]=-1;
 
-        for(int j=0;j<nums2Size;j++){
-            if(nums2[j]==nums1[i]){
-                for(int k=j+1;k<nums2Size;k++){
-                    if(nums2[k]>nums1[i]){
-                        ans[i]=nums2[k];
-                        break;
-                    }
-                }
-                break;
-            }
+    for(int i=0;i<nums2Size;i++){
+        while(top!=-1&&nums2[i]>stack[top]){
+            map[stack[top]]=nums2[i];
+            top--;
         }
+        stack[++top]=nums2[i];
     }
 
+    for(int i=0;i<nums1Size;i++)
+        ans[i]=map[nums1[i]];
+
+    *returnSize=nums1Size;
     return ans;
 }
